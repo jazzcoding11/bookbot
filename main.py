6cede85d69b0e7d271d984173c695e76e740b093
jazzcoding11 <jazzcoding11@gmail.com>
2024-06-15 10:33:36 +0100

    Completed book report

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
     
     char_count = {}
     for char in text:
@@ -20,7 +19,12 @@ def main():
     def count_value(dict_item):
         return dict_item['count']
     report_list.sort(reverse = True, key = count_value)
-    print(report_list)
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document\n")
+    for item in report_list:
+        print(f"The '{item['char']}' character was found {item['count']} times")
+    print("\n--- End report ---")
 
 def get_num_words(text):
     words = text.split()
